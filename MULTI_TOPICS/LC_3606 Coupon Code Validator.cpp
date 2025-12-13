20613004a82a762f11043af950fb4d7b5f89d58e
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2025-12-13 18:11:12 +0530

    LC_3606

--- MULTI_TOPICS/LC_3606 Coupon Code Validator.cpp
+++ MULTI_TOPICS/LC_3606 Coupon Code Validator.cpp
@@ -1,4 +1,4 @@
-// Question Link: 
+// Question Link: https://leetcode.com/problems/coupon-code-validator/description/
 
 // Method 1: simple not optimized method brute force method  main concept used is sorting using lambda function
 
@@ -64,6 +64,68 @@ public:
         for(auto &v : ans){
             finalAns.push_back(v.second);
         }
+        return finalAns;
+    }
+};
+
+
+// Method 2: we dont need lambda function it will happen automatically
+class Solution {
+public:
+    bool correctCode(string code){
+        int n = code.length();
+        if(code.empty()){
+            return false;
+        }
+        for(char c : code){
+            if(!isalnum(c) && c != '_'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool correctBusiness(string business){
+        if((business) == "electronics" || (business) == "grocery" || (business) == "pharmacy" || (business) == "restaurant"){
+            return true;
+        }
+        return false;
+    }
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        int n = code.size();
+        vector<pair<int, string>> ans;        // just changed the input datatype
+
+        for(int i = 0; i < n; i++){
+            if(isActive[i] == true){
+                if(correctCode(code[i]) == true){
+                    if(correctBusiness(businessLine[i]) == true){
+                        pair<int, string> p = {(int)businessLine[i][0], code[i]};    // same here changed the input datatype
+                        ans.push_back(p);
+                    }
+                }
+            }
+        }
+
+        // auto lambda = [](pair<string, string> p1, pair<string, string> p2){
+        //     string business_1 = p1.first;
+        //     string code_1 = p1.second;
+        //     string business_2 = p2.first;
+        //     string code_2 = p2.second;
+
+        //     if(business_1[0] == business_2[0]){
+        //         return p1.second < p2.second;
+        //     }
+
+        //     return business_1[0] < business_2[0];
+        // };
+
+        sort(ans.begin(), ans.end());
+
+        vector<string>finalAns;
+        for(auto &v : ans){
+            finalAns.push_back(v.second);
+        }
+
         return finalAns;
     }
 };
