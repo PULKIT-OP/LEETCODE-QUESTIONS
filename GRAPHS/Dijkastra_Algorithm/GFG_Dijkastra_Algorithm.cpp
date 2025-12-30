4ebd63e710e09e087d80a72a16a08dbcaa7db01f
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2025-12-30 17:44:10 +0530

    Implement Dijkstra's algorithm using set method
    Added a second method for Dijkstra's algorithm using a set to manage costs and nodes, providing an alternative to the priority queue method.

--- GRAPHS/Dijkastra_Algorithm/GFG_Dijkastra_Algorithm.cpp
+++ GRAPHS/Dijkastra_Algorithm/GFG_Dijkastra_Algorithm.cpp
@@ -1,5 +1,6 @@
 // Question Link: 
 
+// METHOD 1: Using PRIORITY QUEUE
 class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
@@ -43,6 +44,58 @@ class Solution {
         }
 
       // then at last return the minDist array which has all then nodes with min cost required to reach
+        return minDist;
+    }
+};
+
+
+// METHOD 2: Using SET : We use this method because in set we have on function which is not available in PRIORITY QUEUE that is 'erase()' function
+// This method is as same as above, just the difference is here we use set and when we get any cost that we have found previously which is greater than current cost in set than we just remove it from the set
+
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Defining SET
+        set<pair<int, int>> st;   //cost, node
+        vector<int> minDist(V, INT_MAX);
+        minDist[src] = 0;
+        st.insert({0, src});
+
+      // making adj
+        vector<vector<pair<int, int>>> adj(V);
+        for(auto &e : edges){
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+            adj[u].push_back({w, v});
+            adj[v].push_back({w, u});
+        }
+
+      // run until set is empty
+        while(!st.empty()){
+            auto &it = *st.begin();
+            int node = it.second;
+            int d = it.first;
+            st.erase(it);
+            
+            // traverse neighbours of 'node'
+            for(auto &v : adj[node]){
+                int dist = v.first;
+                int neigh = v.second;
+                
+                if(d + dist < minDist[neigh]){
+                  // if you find total cost less than previously found cost then check if you have earlier found any cost to reach this node is yes then that would be greater than the cost you have just found
+                  // so you can just remmove that previously found cost from the set because that is of no use now as you have found less cost than that. 
+                  // but still if you want to keep it that would do nothng wrong its on you
+                    if(minDist[neigh] != INT_MAX){
+                      // erasing preiously found cost from the set
+                        st.erase({minDist[neigh], neigh});
+                    }
+                    minDist[neigh] = d + dist;
+                    st.insert({d + dist, neigh});
+                }
+            }
+        }
         return minDist;
     }
 };
