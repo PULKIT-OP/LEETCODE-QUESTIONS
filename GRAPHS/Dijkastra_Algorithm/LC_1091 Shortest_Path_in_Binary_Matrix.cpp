98beff0853422347bf85bbb6fc88ad5122f2de16
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2025-12-31 15:33:33 +0530

    Change priority queue to normal queue in shortest path
    Refactor shortest path algorithm to use a normal queue instead of a priority queue, updating relevant function calls and logic.

--- GRAPHS/Dijkastra_Algorithm/LC_1091 Shortest_Path_in_Binary_Matrix.cpp
+++ GRAPHS/Dijkastra_Algorithm/LC_1091 Shortest_Path_in_Binary_Matrix.cpp
@@ -59,3 +59,64 @@ public:
         return cost[n-1][n-1] + 1;
     }
 };
+
+
+// METHOD 2: Using Normal QUEUE ---> it will work because all weights are same or you can say there are no weights
+
+class Solution {
+public:
+    typedef pair<int, pair<int, int>> P;
+    vector<vector<int>> direction = {
+        {1, 0},   // down
+        {-1, 0},  // up
+        {0, 1},   // right
+        {0, -1},  // left
+        {1, 1},   // bottom-right
+        {1, -1},  // bottom-left
+        {-1, 1},  // top-right
+        {-1, -1}
+    };
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        if(n == 0 || grid[0][0] != 0){
+            return -1;
+        }
+
+        auto isSafe = [&](int x, int y){
+            return x >= 0 && x < n && y >= 0 && y < n;
+        };
+
+        // just changing Priority_Queue to Normal Queue and its function names like front(), rest will be same
+        queue<P> pq;        // {w, {x, y}}
+        pq.push({0, {0, 0}});
+
+        vector<vector<int>> cost(n, vector<int>(n, INT_MAX));
+        cost[0][0] = 0;
+
+        while(!pq.empty()){
+            int c = pq.front().first;
+            int x = pq.front().second.first;
+            int y = pq.front().second.second;
+            pq.pop();
+
+            for(auto &e : direction){
+                int new_x = x + e[0];
+                int new_y = y + e[1];
+                int c2 = 1;
+
+                if(isSafe(new_x, new_y) && grid[new_x][new_y] == 0 && c + c2 < cost[new_x][new_y]){
+                    cost[new_x][new_y] = c + c2;
+                    grid[new_x][new_y] = 1;
+                    pq.push({c + c2, {new_x, new_y}});
+                }
+            }
+        }
+
+        if(cost[n-1][n-1] == INT_MAX){
+            return -1;
+        }
+
+        return cost[n-1][n-1] + 1;
+    }
+};
