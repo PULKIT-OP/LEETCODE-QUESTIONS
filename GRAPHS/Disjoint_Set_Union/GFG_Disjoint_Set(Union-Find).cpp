7b7e522469a13c4df35c9093fd7f39a4b39cfaa2
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2026-01-02 23:56:13 +0530

    Add union by size method to Disjoint Set Union
    Implemented union by size in the Disjoint Set Union (Union-Find) structure.

--- GRAPHS/Disjoint_Set_Union/GFG_Disjoint_Set(Union-Find).cpp
+++ GRAPHS/Disjoint_Set_Union/GFG_Disjoint_Set(Union-Find).cpp
@@ -47,3 +47,41 @@ void unionSet(vector<int> &par, int x, int z, vector<int> &rank) {
         rank[z_par] += 1;
     }
 }
+
+
+
+// METHOD 3: Using SIZE and PATH compression ----> Basic structure of code remains same 
+// instead of increment of rank just when you find same rank we will be adding the size of children node to the parent node while union and when we find same size we do the same here as well
+// Initialize size with 1 for each node.
+// With this method you can find how many nodes are connected to each node
+// Functioning and complexity of RANK-PATH compression and SIZE-PATH compression remains same 
+
+    int find(int x, vector<int> &parent){
+        if(parent[x] == x){
+            return x;
+        }
+        
+        return parent[x] = find(parent[x], parent);
+    }
+    
+    void Union(int x, int y, vector<int> &parent, vector<int> &size){
+        int x_par = find(x, parent);
+        int y_par = find(y, parent);
+        
+        if(x_par == y_par){
+            return;
+        }
+        if(size[x_par] > size[y_par]){
+            parent[y_par] = x_par;
+            size[x_par] += size[y_par];
+        }
+        else if(size[y_par] > size[x_par]){
+            parent[x_par] = y_par;
+            size[y_par] += size[x_par];
+        }
+        else{
+            size[x_par]++;
+            parent[y_par] = x_par;
+            size[x_par] += size[y_par];
+        }
+    }
