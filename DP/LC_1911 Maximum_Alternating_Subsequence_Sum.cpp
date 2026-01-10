1ad0a40c1426718f9968ab740d77a40f7e9f79ae
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2026-01-10 18:24:30 +0530

    Add solution for maximum alternating subsequence sum
    Implemented three methods to calculate the maximum alternating subsequence sum: recursion, memoization, and bottom-up dynamic programming.

---
+++ DP/LC_1911 Maximum_Alternating_Subsequence_Sum.cpp
@@ -0,0 +1,102 @@
+// Question Link: 
+
+// METHOD 1: Recursion Method ----> REHNE DE LALA TLE AAEGA
+// Main logic behind this method is either you can take curr element or you can skip it, but but but if you want to take current element for that you have to more options 
+// Either you will add that to your final answer or you will subtract that number from your final answer
+// To check addition or subtraction you will pass a boolean
+
+class Solution {
+public:
+    long long solve(vector<int> &nums, int i, bool even){
+        if(i >= nums.size()){
+            return 0;
+        }
+      // initializing variables 
+        long long take;
+        long long skip;
+      // if curren index is not even then subtract it from your answer
+        if(!even){
+            take = -nums[i] + solve(nums, i+1, true);
+        }
+      // otherwise add that to your final answer
+        else{
+            take = nums[i] + solve(nums, i+1, false);
+        }
+
+      // if you want to skip than your boolean will be same
+        skip = solve(nums, i+1, even);
+
+      // return maximum of take and skip
+        return max(take, skip);
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+
+        return solve(nums, 0, true);
+    }
+
+};
+
+
+// METHOD 2: MEMOIZATION ---> Logic remains same Just storing repeating answers for future usage
+//
+
+class Solution {
+public:
+// initializing dp vector
+    vector<vector<long long>> dp;
+    long long solve(vector<int> &nums, int i, bool even){
+        if(i >= nums.size()){
+            return 0;
+        }
+      // if you already have best answer for this index then return 
+        if(dp[i][even] != -1){
+            return dp[i][even];
+        }
+        long long take;
+        long long skip;
+        if(!even){
+            take = -nums[i] + solve(nums, i+1, true);
+        }
+        else{
+            take = nums[i] + solve(nums, i+1, false);
+        }
+
+        skip = solve(nums, i+1, even);
+
+        return dp[i][even] = max(take, skip);
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+      // updating dp array size according to question
+        dp.resize(n+1, vector<long long> (2, -1));
+
+        return solve(nums, 0, true);
+    }
+};
+
+
+// METHOD 3: BOTTOM UP
+// Here also logic remains same that we have two choices either take or skip ---> in each iteration we have have two choices either its odd length sequence or even length sequence
+// So for each itereation we are updating two variables odd and even ----> for both of them we are taking maximum of take and skip
+// Assuming 0 ---> even  and   1 ---> odd
+
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+      // initialize dp vector
+        vector<vector<long long>>dp (n+1, vector<long long>(2, 0));
+      // start iteration from 1 to avoid error
+        for(int i = 1; i <= n; i++){
+          // if current index make odd length then take i-1th index of dp and subtract current index, and if you are skipping current index then add i-1th value in current index but with current lenght flag
+          // if current index make even length then take i-1th index of dp and add current index
+            dp[i][1] = max(dp[i-1][0] - nums[i-1], dp[i-1][1]);
+            dp[i][0] = max(dp[i-1][1] + nums[i-1], dp[i-1][0]);
+        }
+
+      // at last index you will have final value so return the maximum value bw even and odd
+        return max(dp[n][0], dp[n][1]);
+    }
+
+};
