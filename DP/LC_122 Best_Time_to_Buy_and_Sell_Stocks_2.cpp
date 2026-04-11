e89730546bd37bc16a242ae17c254d8d0c7816fb
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2026-04-11 17:18:56 +0530

    Add solution for Best Time to Buy and Sell Stocks II
    Implemented three methods to solve the stock buying and selling problem: recursion, memoization, and bottom-up tabulation.

---
+++ DP/LC_122 Best_Time_to_Buy_and_Sell_Stocks_2.cpp
@@ -0,0 +1,98 @@
+// Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+
+// METHOD 1:  RECURRSION - TLE 
+
+class Solution {
+public:
+    int n;
+    int solve(vector<int> &prices, int i, bool bought){
+        if(i == n){
+            return 0;
+        }
+        int profit = 0;
+        if(!bought){
+            int buy = -prices[i] + solve(prices, i+1, true);
+            int nothing = 0 + solve(prices, i+1, bought);
+            profit = max(buy, nothing);
+        }
+        else{
+            int sell = prices[i] + solve(prices, i+1, false);
+            int nothing = 0 + solve(prices, i+1, bought);
+            profit = max(sell, nothing);
+        }
+
+        return profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        n = prices.size();
+
+        return solve(prices, 0, 0);
+    }
+};
+
+
+// MEMOIZAION : 
+
+class Solution {
+public:
+    int n;
+    int dp[30001][2];
+    int solve(vector<int> &prices, int i, bool bought){
+        if(i == n){
+            return 0;
+        }
+
+        if(dp[i][bought] != -1){
+            return dp[i][bought];
+        }
+        int profit = 0;
+        if(!bought){
+            int buy = -prices[i] + solve(prices, i+1, true);
+            int nothing = 0 + solve(prices, i+1, bought);
+            profit = max(buy, nothing);
+        }
+        else{
+            int sell = prices[i] + solve(prices, i+1, false);
+            int nothing = 0 + solve(prices, i+1, bought);
+            profit = max(sell, nothing);
+        }
+
+        return dp[i][bought] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        n = prices.size();
+        memset(dp, -1, sizeof(dp));
+
+        return solve(prices, 0, 0);
+    }
+};
+
+
+// BOTTOM UP - TABULATION : 
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+
+        int profit = 0;
+        for(int i = n-1; i >= 0; i--){
+            for(int bought = 0; bought < 2; bought++){
+                if(bought == 0){
+                    int buy = -prices[i] + dp[i+1][1-bought];
+                    int nothing = 0 + dp[i+1][bought];
+                    profit = max(buy, nothing);
+                }
+                else{
+                    int sell = prices[i] + dp[i+1][1-bought];
+                    int nothing = 0 + dp[i+1][bought];
+                    profit = max(sell, nothing);
+                }
+                dp[i][bought] = profit;
+            }
+        }
+
+        return dp[0][0];
+    }
+};
