099410866202181f90a7de23df0ce2238623fd21
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2026-06-01 13:42:03 +0530

    Implement selection sort algorithm

---
+++ SORTING/ALGORITHMS/GFG_Selection_Sort.cpp
@@ -0,0 +1,21 @@
+// Question Link: https://www.geeksforgeeks.org/problems/selection-sort/1
+
+// Selection Sort ----> SELECTION OF SMALLEST then swap with starting node then move forward
+class Solution {
+	public:
+	// Function to perform selection sort on the given array.
+	void selectionSort(vector<int> &arr) {
+		// code here
+		int n = arr.size();
+		int min = 0;
+		for (int i = 0; i < n; i++) {
+		    min = i;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+			swap(arr[i], arr[min]);
+		}
+	}
+};
