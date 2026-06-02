499f7a2f190ad3a7c460758916c03a6507ac0458
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2026-06-02 23:03:53 +0530

    Create LC_81_Search_in_Rotated_Array_2.cpp

---
+++ BINARY SEARCH/LC_81_Search_in_Rotated_Array_2.cpp
@@ -0,0 +1,47 @@
+// Question Link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+
+// METHOD 1: Just like the first part with a little condition 
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size() - 1;
+
+        // checking which half is sorted and of my concern in finding target
+        while(s <= e){
+            int mid = s + (e - s)/2;
+            if(target == nums[mid]){
+                return true;
+            }
+
+          // if duplicated encountered then shrink the array and continue
+            if(nums[s] == nums[mid] && nums[mid] == nums[e]){
+                s++;
+                e--;
+                continue;
+            }
+
+          // left half sorted???
+            if(nums[s] <= nums[mid]){
+                if(target <= nums[mid] && target >= nums[s]){
+                    e = mid - 1;    // forget the right half we got the valuable part of array
+                }
+                else{
+                    s = mid + 1;    // forget the left half we got the valuable part of array
+                }
+            }
+          // Right half sorted??
+            else{
+                if(target > nums[mid] && target <= nums[e]){
+                    s = mid + 1;    // forget the left half we got the valuable part of array
+                }
+                else{
+                    e = mid - 1;    // forget the right half we got the valuable part of array
+                }
+            }
+        }
+
+        return false;
+    }
+};
