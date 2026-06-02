fb693f7c0320190437f865fcac3769ad3838ca1d
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2026-06-02 19:28:51 +0530

    Create GFG_Implement_Lower_Bound.cpp

---
+++ BINARY SEARCH/GFG_Implement_Lower_Bound.cpp
@@ -0,0 +1,40 @@
+// Question Link: https://www.geeksforgeeks.org/problems/implement-lower-bound/1
+
+
+// METHOD 1: Using Binary Search with a tweak
+
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        // code here
+        int s = 0;
+        int e = arr.size() - 1;
+        
+        int idx = INT_MAX;
+        
+        while(s <= e){
+            int mid = s + (e - s)/2;
+            
+            if(arr[mid] == target){
+                if(mid < idx){
+                    idx = mid;
+                }
+                e = mid - 1;    // -----> The TWEAK it needs
+            }
+            else if(arr[mid] > target){
+                if(mid < idx){
+                    idx = mid;
+                }
+                e = mid - 1;
+            }
+            else if(arr[mid] < target){
+                s = mid + 1;
+            }
+            
+        }
+        if(idx == INT_MAX){
+            return arr.size();
+        }
+        return idx;
+    }
+};
