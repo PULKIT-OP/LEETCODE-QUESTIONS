304f549dea7a3d89567aba2b22ba324e7713df9b
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2026-06-10 22:31:15 +0530

    Update LC_144_Binary_Tree_Preorder_Traversal.cpp

--- BINARY TREES/LC_144_Binary_Tree_Preorder_Traversal.cpp
+++ BINARY TREES/LC_144_Binary_Tree_Preorder_Traversal.cpp
@@ -20,3 +20,33 @@ public:
         return pre;
     }
 };
+
+// MEHTOD 2: Iterative Method with Stack
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preOrder;
+        if(root == NULL){
+            return preOrder;
+        }
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            preOrder.push_back(root -> val);
+            if(root -> right){
+                st.push(root -> right);
+            }
+            if(root -> left){
+                st.push(root -> left);
+            }
+        }
+        return preOrder;
+    }
+};
+
+
