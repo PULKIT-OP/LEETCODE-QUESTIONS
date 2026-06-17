f88ef97841e50bccec2fdfb41673f7fd372fb824
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2026-06-17 23:02:03 +0530

    Update LC_542 01_Matrix.cpp

--- GRAPHS/LC_542 01_Matrix.cpp
+++ GRAPHS/LC_542 01_Matrix.cpp
@@ -28,18 +28,20 @@ public:
             int size = que.size();
 
             while(size--){
+                // the parent cell index 
                 int x = que.front().first;
                 int y = que.front().second;
                 que.pop();
 
-              // iterating over neighbours
+              // iterating over neighbours of parent cell
                 for(auto &e : direction){
                     int new_x = x + e.first;
                     int new_y = y + e.second;
-                  // if you get any neighbour which is -1 it means it is having one so update its size and push in que
+                  // if you get any neighbour which is -1 it means it's having 1 
+                  // we know the steps to reach parent cell so just add 1 to it and update this current neighbour
                     if(new_x >= 0 && new_x < n && new_y >= 0 && new_y < m && sizeMat[new_x][new_y] == -1){
-                        sizeMat[new_x][new_y] = sizeMat[x][y] + 1;
-                        que.push({new_x, new_y});
+                        sizeMat[new_x][new_y] = sizeMat[x][y] + 1;    // updating neighbour by adding one step to parent cell
+                        que.push({new_x, new_y});    // And now push this to q so that we can traveerse to its neighbour in future
                     }
                 }
             }
