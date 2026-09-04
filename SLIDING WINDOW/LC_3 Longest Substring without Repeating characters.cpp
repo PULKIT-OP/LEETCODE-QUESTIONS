34017de7d59098af93fe91e565ef530a8986e4a9
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2026-09-04 23:14:14 +0530

    Update LC_3 Longest Substring without Repeating characters.cpp

--- SLIDING WINDOW/LC_3 Longest Substring without Repeating characters.cpp
+++ SLIDING WINDOW/LC_3 Longest Substring without Repeating characters.cpp
@@ -21,3 +21,30 @@ int lengthOfLongestSubstring(string s) {
     }
     return maxi;
 }
+
+// Clean and readable method
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+
+        int i = 0; 
+        int j = 0;
+        int ans = 0;
+        unordered_map<char, int> seen;
+        while(j < n){
+            // if you have seen current element before then remove it from current set
+            while(seen[s[j]] > 0){
+                seen[s[i]]--;
+                i++;
+            }
+            // mark current element seen and store the max length of array we have found till now
+            seen[s[j]]++;
+            ans = max(ans, j-i+1);
+            j++;    // increment j
+        }
+
+        return ans;
+    }
+};
